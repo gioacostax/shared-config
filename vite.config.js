dea722e4f88c66f0ac1daea97002352b3656caaf
gioacostax <gioacostax@gmail.com>
2024-09-16 08:50:30 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 /**
- * This file can be used to define Vite configuration,
+ * Project vite (base-config)
  * more info in https://vitejs.dev/config/
  */
 
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => ({
+export default defineConfig({
   build: {
     assetsDir: '.',
     outDir: 'build',
@@ -13,20 +13,9 @@ export default defineConfig(() => ({
       output: {
         assetFileNames: 'assets/[hash].[ext]',
         chunkFileNames: 'js/[hash].js',
-        entryFileNames: 'main-[hash].js',
+        entryFileNames: 'index-[hash].js',
+        manualChunks: (id) => id.includes('node_modules') && 'vendor',
       },
     },
   },
-  server: {
-    port: 3200,
-    // proxy: {
-    //   '/proxy/services': {
-    //     rewrite: (path) => path.replace(/^\/proxy\/services/, ''),
-    //     target: 'https://target...',
-    //     changeOrigin: true,
-    //     secure: false,
-    //     cookieDomainRewrite: 'localhost',
-    //   },
-    // },
-  },
-}));
+});
