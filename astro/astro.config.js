0bf1d9536c96d4f1b5f77ac71cfadb892b7ebde0
gioacostax <gioacostax@gmail.com>
2024-10-31 15:05:45 -0500

    inline styles

--- astro/astro.config.js
+++ astro/astro.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
   build: {
     assets: 'assets',
     format: 'file',
+    inlineStylesheets: 'always',
   },
   integrations: [tailwind(), sitemap()],
   markdown: {
