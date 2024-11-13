5a3a39d94cec32f0a0d9e341211351f87f92bb96
gioacostax <gioacostax@gmail.com>
2024-11-13 12:40:49 -0500

    fix vite https config

--- vite/https.vite.config.js
+++ vite/https.vite.config.js
@@ -3,22 +3,25 @@
  * more info in https://vitejs.dev/config/
  */
 
+import { defineConfig } from 'astro/config';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
 
 /**
  * Vite config dependencies
  * - vite
  */
 /** @type (host: string, path?: string, scriptName?: string) => import('vite').UserConfig */
-export default (host, path = './cert', scriptName = 'dev:https') =>
+export default (host, path = './.cert', scriptName = 'dev:https') =>
   defineConfig({
-    ...(process.env.npm_lifecycle_event === scriptName && {
-      host,
-      https: {
-        cert: readFileSync(resolve(path, `${host}.pem`)),
-        key: readFileSync(resolve(path, `${host}-key.pem`)),
-      },
-    }),
+    server: {
+      ...(process.env.npm_lifecycle_event === scriptName && {
+        host,
+        https: {
+          cert: readFileSync(resolve(path, `${host}.crt`)),
+          key: readFileSync(resolve(path, `${host}.key`)),
+        },
+        port: 4321,
+      }),
+    },
   });
