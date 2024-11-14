ab2339b5d02c8416ee45ab2cd211cd13766dcb4c
gioacostax <gioacostax@gmail.com>
2024-11-14 15:44:07 -0500

    https config

--- vite/https.vite.config.js
+++ vite/https.vite.config.js
@@ -5,23 +5,19 @@
 
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
 
 /**
  * Vite config dependencies
  * - vite
  */
-export default (host, port = 4321, path = './.cert', scriptName = 'dev:https') =>
-  /** @type (host: string, path?: string, scriptName?: string) => import('vite').UserConfig */
-  defineConfig({
-    server: {
-      ...(process.env.npm_lifecycle_event === scriptName && {
-        host,
-        https: {
-          cert: readFileSync(resolve(path, `${host}.crt`)),
-          key: readFileSync(resolve(path, `${host}.key`)),
-        },
-        port,
-      }),
+/** @type (host: string, port?: number, path?: string, scriptName?: string) => object */
+export default (host, port = 4321, path = './.cert', scriptName = 'dev:https') => ({
+  ...(process.env.npm_lifecycle_event === scriptName && {
+    host,
+    https: {
+      cert: readFileSync(resolve(path, `${host}.crt`)),
+      key: readFileSync(resolve(path, `${host}.key`)),
     },
-  });
+    port,
+  }),
+});
